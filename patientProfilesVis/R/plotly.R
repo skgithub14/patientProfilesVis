95eeb496eb30e6780f763ac0df07e77c81582cb3
Stephen Knapp <stephen@knappconsultingllc.com>
2024-07-17 21:59:24 +0000

    plotly lineplot checkpoint

--- patientProfilesVis/R/plotly.R
+++ patientProfilesVis/R/plotly.R
@@ -1,37 +1,209 @@
-#' Create a line plot for one subject
+#' Create plotly tool tip hover template for a line plot
 #'
+#' @details 
+#' This function is designed to called within a [plotly::plot_ly()] or
+#' [plotly::add_trace()] call for the `hovertemplate` argument.
+#'
+#' @param title a string, the tool tip title, ideally this is the parameter with
+#'   units appended
+#' @inheritParams patientProfilesVis-common-args
+#' @inheritParams subjectProfileLinePlot
+#' @param colorVal an optional string or numeric value, the data value that
+#'   corresponds to the current color
+#' @param shapeVal an optional string or numeric value, the data value that
+#'   corresponds to the current shape
+#' @param visit an optional string, the visit name
+#' @param date an optional string or date, the date
+#' @param lln,uln optional numeric values, the lower and upper limit normal
+#'   values, respectively
+#' @param grade an optional string or numeric value, the grade. If value is `""`
+#'   or `NA`, grade will not be included in the hover template.
+#' @param alert an optional string, the alerts criteria violated. If value is
+#'   `""` or `NA`, grade will not be included in the hover template.
+#'
+#' @returns a string with html formatting
+#' 
+linePlotHoverTemplate <- function(title, 
+                                  timeLab, 
+                                  paramValueVar, 
+                                  colorLab = NULL, 
+                                  colorVal = NULL, 
+                                  shapeLab = NULL, 
+                                  shapeVal = NULL,
+                                  visit = NULL,
+                                  date = NULL,
+                                  lln = NULL,
+                                  uln = NULL,
+                                  grade = NULL,
+                                  alert = NULL) {
+  
+  # title (parameter with units), value, and study day
+  ht <- paste0(
+    '<b>', title, '</b><br><br>',
+    '<i>', paramValueVar, '</i>: %{y}<br>',
+    '<i>', timeLab, '</i>: %{x}<br>'
+  )
+  
+  ## additional timing variables
+  # visit
+  if (!is.null(visit)) ht <- paste0(ht, '<i>Visit</i>: ', visit, '<br>')
+  
+  # date
+  if (!is.null(date)) ht <- paste0(ht, '<i>Date</i>: ', date, '<br>')
+  
+  ## additional aesthetic variables
+  # color
+  if (!is.null(colorLab) & !is.null(colorVal)) {
+    ht <- paste0(ht, '<i>', colorLab, '</i>: ', colorVal, '<br>')
+  }
+  
+  # shape
+  if (!is.null(shapeLab) & !is.null(shapeVal)) {
+    ht <- paste0(ht, '<i>', shapeLab, '</i>: ', shapeVal, '<br>')
+  }
+  
+  # ranges
+  if (!is.null(uln)) ht <- paste0(ht, '<i>ULN</i>: ', uln, '<br>')
+  if (!is.null(lln)) ht <- paste0(ht, '<i>LLN</i>: ', lln, '<br>')
+  
+  ## value interpretation variables
+  # grade
+  if (!is.null(grade)) {
+    if (!is.na(grade) & grade != "") {
+      ht <- paste0(ht, '<i>Grade</i>: ', grade, '<br>')
+    }
+  }
+  
+  # alert
+  if (!is.null(alert)) {
+    if (!is.na(alert) & alert != "") {
+      ht <- paste0(ht, '<i>Alert</i>: ', alert, '<br>')
+    }
+  }
+  
+  ht <- paste0(ht, '<extra></extra>')
+  return(ht)
+}
+
+
+
+#' Convert [ggplot] shape to [plotly] symbol
+#'
+#' @param shapes a numeric or character vector representing [ggplot2] shapes
+#'
+#' @returns a string, the corresponding plotly shape name
+#'
+convert_ggplot_shapes_to_plotly_symbols <- function(shapes) {
+  
+  convert_shape <- function(shape) {
+    
+    # convert ggplot shape from character to numeric
+    if (!grepl("^[[:digit:]]+L*$", shape)) {
+      shape <- ggplot2::translate_shape_string(shape)
+    }
+    
+    shape <- as.character(shape)
+    switch(
+      shape,
+      "0" = "square-open",
+      "1" = "circle-open",
+      "2" = "triangle-up-open",
+      "3" = "cross-thin",
+      "4" = "x-thin",
+      "5" = "diamond-open",
+      "6" = "triangle-down-open",
+      "7" = "square-x-open",
+      "8" = "asterisk-open",
+      "9" = "diamond-cross-open",
+      "10" = "circle-cross-open",
+      "11" = "hourglass-open",
+      "12" = "square-cross-open",
+      "13" = "circle-x-open",
+      # "14" # no equivalent
+      "15" = "square-dot", 
+      "16" = "circle-dot", 
+      "17" = "triangle-up",
+      "18" = "diamond-dot",
+      "19" = "circle-dot",
+      "20" = "circle-dot",
+      "21" = "circle-dot",
+      "22" = "square-dot",
+      "23" = "diamond-dot",
+      "24" = "triangle-up",
+      "25" = "triangle-down",
+      stop(paste("no plotly shape corresponds to ggplot shape", shape))
+    )
+  }
+  
+  purrr::map_chr(shapes, \(shape) convert_shape(shape))
+}
+
+
+#' Create a plotly line plot facetted by paramFacetVar
+#' 
+#' @details 
+#' This function is designed to be called by [subjectProfileLinePlot()].
+#'
+#' @param data a data frame with data for 1 subject only
+#' @inheritParams patientProfilesVis-common-args
+#' @inheritParams subjectProfileLinePlot
+#' @param visitVar an optional string, the column name in `data` with the visit 
+#'   name
+#' @param dateVar an optional string, the column name in `data` with the date
+#' @param gradeVar an optional string, the column name in `data` with the grade
+#' @param alertVar an optional string, the column name in `data` with the alert 
+#'   information
+#' @param facetVarMaxLength a numeric value, the maximum number of characters 
+#'   allowed in an entry in the `data$paramFacetVar` column before line 
+#'   breaks will be added at every space
+#' @param margin a named list of numeric values specifying the plot margins,
+#'   names should be `l` (left), `r` (right), `b` (bottom), `t` (top), and `pad`
+#'   (padding); default is `list(l = 250, r = 50, b = 75, t = 50, pad = 4)`. 
+#'   This argument, particularly the left margin, can be adjusted along with 
+#'   `yaxis_title_shift` to adjust the y-axis title location
+#' @param yaxis_title_shift a numeric value, adjust y-axis title offset, works
+#'   with `margin` argument
+#' @param spikecolor a string, the spike line color; default is `'red'`
+#' 
 #' @returns a plotly object
 #' @export
 #'
-plotlyLinePlot <- function(data) {
+plotlyLinePlot <- function(data,
+                           paramValueVar,
+                           paramValueRangeVar = NULL,
+                           colorValueRange,
+                           colorVar = NULL,
+                           colorLab = NULL,
+                           colorPalette,
+                           alpha,
+                           shapeVar = NULL,
+                           shapeLab = NULL,
+                           shapePalette = NULL,
+                           shapeSize,
+                           timeVar,
+                           timeLab,
+                           xLab,
+                           title,
+                           visitVar = NULL,
+                           dateVar = NULL,
+                           gradeVar = NULL,
+                           alertVar = NULL,
+                           facetVarMaxLength = 30,
+                           margin = list(
+                             l = 250,
+                             r = 50,
+                             b = 75,
+                             t = 50,
+                             pad = 4
+                           ),
+                           yaxis_title_shift = -0.035,
+                           spikecolor = 'red') {
   
-  # inputs
-  data <- dataLB
-  paramNameVar = "LBTEST"
-  paramValueVar = "LBSTRESN"
-  data[, "yVar"] <- data[, paramValueVar]
-  paramGroupVar = "LBCAT"
-  paramValueRangeVar = c("LBSTNRLO", "LBSTNRHI")
-  colorValueRange = "lightgreen"
-  paramVarSep = "-"
-  data[, "paramFacetVar"] <- interactionWithMissing(data = data, vars = paramNameVar, varSep = paramVarSep)
-  colorVar = "LBCAT"
-  colorPalette <- getColorPalettePatientProfile(x = data[, colorVar])
-  colorLab = "Lab Category"
-  shapeVar = "LBNRIND"
-  shapePalette = c(
-    'LOW' = 25, 'NORMAL' = 19, 'HIGH' = 24, 
-    'ABNORMAL' = 11
-  )
-  shapeSize <- 7
-  alpha = 1
-  timeVar = "LBDY"
-  xLab = "Study Day"
-  yLab = ""
-  title = "Laboratory test measurements: actual value"
-  labelVars = labelVarsSDTM
   
-  # colors
+  browser()
+  
+  
+  # format ribbon colors
   ribbon_color <- col2rgb(colorValueRange)
   ribbon_color <- paste0("rgba(", 
                          ribbon_color[1,1], ",",
@@ -39,15 +211,16 @@ plotlyLinePlot <- function(data) {
                          ribbon_color[3,1], ",",
                          "0.2)")
   
-  # shapes
-  shapePalette <- getShapePalettePatientProfile(x = data[, shapeVar])
-  data <- dplyr::mutate(
-    data,
-    shapeVarSymbol = dplyr::recode(!!rlang::sym(shapeVar), !!!shapePalette)
-  )
+  # create a shape symbol column in the data
+  if (!is.null(shapeVar)) {
+    # data <- dplyr::mutate(
+    #   data,
+    #   shapeVarSymbol = dplyr::recode(!!rlang::sym(shapeVar), !!!shapePalette)
+    # )
+    shapePalettePlotly <- convert_ggplot_shapes_to_plotly_symbols(shapePalette)
+  }
   
-  # y axis labels - if too long, break into lines
-  facetVarMaxLength <- 30
+  # format y axis labels if too long
   paramFacetVarLevels <- levels(data$paramFacetVar)
   paramFacetVarLevels <- purrr::map_chr(paramFacetVarLevels, \(x) {
     if (nchar(x) > facetVarMaxLength) {
@@ -68,21 +241,9 @@ plotlyLinePlot <- function(data) {
                            levels = paramFacetVarLevels)
   )
   
-  fixer <- function(plt) {  
-    
-    
-    
-    plt <- plotly::plotly_build(plt)
-    print(plt)
-    
-    # plt$x$data[[17]]$showlegend <- TRUE
-    # 
-    # p
-  }
-  
   # plot
-  p <- data %>%
-    dplyr::group_by(dplyr::across(tidyselect::all_of(paramNameVar))) %>%
+  plot_tbl <- data %>%
+    dplyr::group_by(paramFacetVar) %>%
     plotly::do(
       p = plotly::plot_ly(
         .,
@@ -92,14 +253,38 @@ plotlyLinePlot <- function(data) {
         plotly::add_trace(
           type = "scatter",
           mode = "markers",
-          symbol = ~shapeVarSymbol,
+          symbol = ~shapeVar,
+          symbols = shapePalettePlotly,
           marker = list(
-            color = colorPalette[which(names(colorPalette) == unique(.data[[colorVar]]))],
+            color = if (!is.null(colorVar)) {
+              colorPalette[
+                which(names(colorPalette) == unique(.data[[colorVar]]))
+              ]
+            } else {
+              colorPalette # variable is never NULL in subjectProfileLinePlot
+            },
             size = shapeSize,
             opacity = alpha
           ),
-          legendgroup = ~yVar,
           showlegend = FALSE
+          # ,
+          # hovertemplate = linePlotHoverTemplate(
+          #   # title = unique(.data$paramFacetVar),
+          #   title = .data$paramFacetVar,
+          #   timeLab = timeLab,
+          #   paramValueVar = paramValueVar,
+          #   colorLab = colorLab,
+          #   # colorVal = unique(.data[[colorVar]]),
+          #   colorVal = .data[[colorVar]],
+          #   shapeLab = shapeLab,
+          #   shapeVal = .data[[shapeVar]],
+          #   visit = .data[[visitVar]],
+          #   date = .data[[dateVar]],
+          #   lln = .data[[paramValueRangeVar[1]]],
+          #   uln = .data[[paramValueRangeVar[2]]],
+          #   grade = .data[[gradeVar]],
+          #   alert = .data[[alertVar]]
+          # )
         ) %>%
         plotly::add_trace(
           type = "scatter",
@@ -110,15 +295,23 @@ plotlyLinePlot <- function(data) {
           ),
           showlegend = FALSE
         ) %>%
-        plotly::add_ribbons(
-          ymin = ~.data[[paramValueRangeVar[1]]],
-          ymax = ~.data[[paramValueRangeVar[2]]],
-          line = list(
-            color = ribbon_color
-          ),
-          fillcolor = ribbon_color,
-          showlegend = FALSE
-        ) %>%
+        {
+          if (!is.null(paramValueRangeVar)) {
+            plotly::add_ribbons(
+              .,
+              ymin = ~.data[[paramValueRangeVar[1]]],
+              ymax = ~.data[[paramValueRangeVar[2]]],
+              line = list(
+                color = ribbon_color
+              ),
+              fillcolor = ribbon_color,
+              showlegend = FALSE,
+              hoverinfo = 'none'
+            )
+          } else {
+            .
+          }
+        } %>%
         plotly::layout(
           title = title,
           xaxis = list(
@@ -127,30 +320,45 @@ plotlyLinePlot <- function(data) {
           yaxis = list(
             title = NA
           ),
-          margin = list(
-            l = 250,
-            r = 50,
-            b = 75,
-            t = 50,
-            pad = 4
+          margin = margin,
+          hoverlabel = list(
+            align = "left"
           )
         ) %>%
         plotly::add_annotations(
-          text = unique(.data$paramFacetVar),
+          # text = unique(.data$paramFacetVar),
+          text = .data$paramFacetVar,
           xref = "paper",
           yref = "paper",
-          x = -0.05,
+          x = yaxis_title_shift,
           y = 0.5,
           xanchor = "right",
           yanchor = "bottom",
           showarrow = FALSE
         )
-    ) %>%
-    plotly::subplot(
-      nrows = length(unique(data[[paramNameVar]])),
-      shareX = TRUE,
-      shareY = TRUE
     )
-    
   
+  plots <- plotly::subplot(
+    plot_tbl,
+    nrows = length(unique(data$paramFacetVar)),
+    shareX = TRUE,
+    shareY = TRUE
+  ) %>%
+  plotly::layout(
+    xaxis = list(
+      spikemode = 'across',
+      spikecolor = spikecolor
+    )
+  ) %>%
+  plotly::config(
+    displaylogo = FALSE,
+    modeBarButtonsToRemove = c(
+      'lasso2d',
+      'select2d',
+      'hoverClosestCartesian',
+      'hoverCompareCartesian'
+    )
+  )
+    
+  return(plots)
 }
